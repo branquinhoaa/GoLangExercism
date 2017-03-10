4d250bc6e78267b8947a90669323a939d50fb560
branquinhoaa <amanda.alves15@gmail..com>
2017-03-10 15:02:08 -0300

    hamming exercise

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,7 +1,19 @@
 package hamming
 
+import "errors"
+
 const testVersion = 5
 
 func Distance(a, b string) (int, error) {
-	// your code here!
+	dist := 0
+
+	if len(a) != len(b) {
+		return -1, errors.New("strings from different sizes")
+	}
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			dist += 1
+		}
+	}
+	return dist, nil
 }
