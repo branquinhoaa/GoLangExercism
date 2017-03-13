3fe0e9124795908757635da55ff57951590fb6a9
branquinhoaa <amanda.alves15@gmail..com>
2017-03-13 15:51:12 -0300

    include pangram resolution

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,19 @@
 package pangram
 
+import "strings"
+
 const testVersion = 1
 
-func IsPangram(string) bool
+func IsPangram(str string) bool {
+	alphabet := strings.Split("abcdefghijklmnopqrstuvxwyz", "")
+	inpuText := strings.ToLower(str)
+
+	for _, letter := range alphabet {
+		if strings.ContainsAny(letter, inpuText) {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
